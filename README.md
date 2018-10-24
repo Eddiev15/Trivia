f90517614a5997f0787f7b5b08957b7c1fb05c4b
Eddiev15 <eddie.vilchez15@gmail.com>
2018-10-24 15:54:38 -0400

    Updating Files

---
+++ README.md
@@ -0,0 +1,25 @@
+# trivia-game
+
+In this assignment, we were required to create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML. 
+
+## Getting Started
+
+**NBA Trivia**
+
+Instructions:
+
+Click Start to begin the trivia.
+You have 30 seconds to answer each question and a counter will be displayed to show how much time is reamining.
+The answer to each question is displayed afterwards whether you selected the correct answer, the incorrect answer or if no answer is selected before time ran out.
+If no answer is selected before the timer runs out, the trivia will advance to the next question.
+At the end of the game, you will be shown the number of correct answers, the number of incorrect answers and any questions that went unanswered.
+
+## Built With
+
+* HTML5
+* CSS3
+* [Twitter-Bootstrap](http://getbootstrap.com/) - Framework
+* JavaScript 
+* [jQuery](https://api.jquery.com/) - JavaScript Library
+
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,227 @@
+// Define variables
+$(document).ready(function(){
+    var count = 0;
+    var time = 31;
+    var isSelected = false;
+    var ticker;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+// Questions and Answer Arrays
+    var question = ["What hoopster scored in double figures in 787 straight games, from December 4, 1977 to December 4, 1987?",
+    "What NBA team plays home games in an arena located at Two Pennsylvania Plaza?", "What NBA hoopster averaged an amazing 48.5 minutes per game, including overtime, in the 1961-62 season?", "What seven-foot-one NBA center's first name translates as 'little one'?", "What was the first NBA team to win 70 games or more in the regular season?",
+    "How many NBA titles did Magic Johnson help the Lakers win as a player?", " What city decided to call its new NBA team the Grizzlies, after much debate?", "What franchise has played in the most NBA finals since 1947?", "Who was the first hoopster to win eight NBA scoring titles?", "What NBA team is named after a car part?"];
+    var answer = ["Kareem Abdul-Jabbar", "The New York Knicks", "Wilt Chamberlin", "Shaquille O'Neal", "Chicago Bulls", "Five", "Vancouver", "The Lakers"];
+    var firstChoice = ["Shaquille O'Neal", "Philadelphia 76ers", "Michael Jordan", "Kareem Abdul-Jabbar", "Miami Heat", "Ten", "Boston", "The Pistons"];
+    var secondChoice = ["Dikembe Mutombo", "The New York Knicks", "Wilt Chamberlin", "Wilt Chamberlain", "Los Angeles Lakers", "New York", "The Timberwolves"];
+    var thirdChoice = ["Patrick Ewing", "Detroit Pistons", "Magic Johnson", "Shaquille O'Neal", "Chicago Bulls", "Two", "Vancouver", "The Bucks"];
+    var fourthChoice = ["Kareem Abdul-Jabbar", "Chicago Bulls", "Larry Bird", "Dirk Nowitski", "Golden State Warriors", "Houston", "The Lakers"];
+
+// Show & Hide Functions
+    function showHolders() {
+        $("#question-holder").show();
+        $("#choice-holder-1").show();
+        $("#choice-holder-2").show();
+        $("#choice-holder-3").show();
+        $("#choice-holder-4").show();
+    }
+    function hideHolders() {
+        $("#question-holder").hide();
+        $("#choice-holder-1").hide();
+        $("#choice-holder-2").hide();
+        $("#choice-holder-3").hide();
+        $("#choice-holder-4").hide();
+    }
+    function hideResults() {
+        $("#correct-holder").hide();
+        $("#incorrect-holder").hide();
+        $("#unanswered-holder").hide();
+        $("#restart-holder").hide();
+    }
+    function displayQuestion () {
+        hideResults();
+        $("#answer-holder").hide();
+        $("#image-holder").hide();
+        $("#time-holder").show();
+        showHolders();
+        $("#question-holder").html(question[count]);
+        $("#choice-holder-1").html(firstChoice[count]);
+        $("#choice-holder-2").html(secondChoice[count]);
+        $("#choice-holder-3").html(thirdChoice[count]);
+        $("#choice-holder-4").html(fourthChoice[count]);
+    
+    // Hover CSS
+        $("#choice-holder-1").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-2").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-3").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-4").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+    }
+    $("#choice-holder-1").on("click", checkAnswer) 
+    $("#choice-holder-2").on("click", checkAnswer)
+    $("#choice-holder-3").on("click", checkAnswer)
+    $("#choice-holder-4").on("click", checkAnswer)
+
+// Check Answer Function
+    function checkAnswer() {
+
+        hideHolders();
+
+        if($(this).text() === answer[count]) {
+            stopTime();
+            isSelected = true;
+            $("#answer-holder").show();
+            $("#answer-holder").html("Right! The answer is: " + answer[count]);
+            displayImage();
+            correct++;
+            count++;
+        }
+        else {
+            stopTime();
+            isSelected = true;
+            $("#answer-holder").show();
+            $("#answer-holder").html("Wrong! The answer is: " + answer[count]);
+            displayImage();
+            incorrect++;
+            count++;
+        } 
+
+        checkGameEnd();  
+    }
+
+// Chekc End Game Function
+    function checkGameEnd() {
+        if(count === question.length) {
+            $("#time-holder").hide();
+            showResults();
+            count = 0;
+            $(".start").show();
+            $(".start").on("click", function() {
+                resetResults();
+                startGame();
+            });
+        }
+    }
+
+    function resetTime() {
+        time = 31;
+    }
+
+    function displayTime() {
+        time--;
+        $("#time-holder").html("Time remaining: " + time);
+      
+            if(time <= 0) {
+                hideHolders();
+                stopTime();
+                $("#answer-holder").show();
+                $("#answer-holder").html("Time is up! The answer is: " + answer[count]);
+                displayImage();
+                unanswered++;
+                count++;
+                checkGameEnd();
+            }
+    }
+
+    function startTime() {
+        clearInterval(ticker);
+        ticker = setInterval(displayTime, 1000);
+    }
+    function stopTime() {
+        clearInterval(ticker);
+        resetTime();
+        if(count < question.length - 1) {
+            setTimeout(startTime, 2000);
+            setTimeout(displayQuestion, 3000);
+        }
+    }
+
+    resetTime();
+
+// Display Images With Answer
+    function displayImage() {
+        if(count === 0) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/kareem.jpeg">');
+        }
+        else if(count === 1) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/knicks.png">');
+        }
+        else if(count === 2) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/wilt.jpg">');
+        }
+        else if(count === 3) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/shaq.png">');
+        }
+        else if(count === 4) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/bulls.png">');
+        }
+        else if(count === 5) {
+            $("#image-holder").show();
+            $("#image-holder").html('');
+        }
+        else if(count === 6) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/vancouver.jpg">');
+        }
+        else if(count === 7) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/lakers.jpg">');
+        }
+    }
+
+ // Show Results Function   
+    function showResults() {
+        $("#correct-holder").show();
+        $("#correct-holder").html("Correct: " + correct);
+        $("#incorrect-holder").show();
+        $("#incorrect-holder").html("Incorrect: " + incorrect);
+        $("#unanswered-holder").show();
+        $("#unanswered-holder").html("Unanswered: " + unanswered);
+        $("#restart-holder").show();
+        $("#restart-holder").html("Click Start above to play again!");
+    }
+
+// Reset Results Function 
+    function resetResults() {
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+    }
+
+// Start Game Function
+    function startGame() {
+        $(".start").hide();
+        startTime();
+        displayQuestion();
+    }
+
+// Start Game On Click
+  $(".start").on("click", function() {
+    startGame();
+  });
+});
